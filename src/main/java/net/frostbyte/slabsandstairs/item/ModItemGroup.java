7ca42edcfb02d36379a56aa4f7e83ff4a63acc8d
Frostbyte Games <109047591+FrostbyteGames1@users.noreply.github.com>
2022-07-16 15:22:28 -0400

    Updated creative mode tab icon

--- src/main/java/net/frostbyte/slabsandstairs/item/ModItemGroup.java
+++ src/main/java/net/frostbyte/slabsandstairs/item/ModItemGroup.java
@@ -2,6 +2,7 @@ package net.frostbyte.slabsandstairs.item;
 
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.frostbyte.slabsandstairs.SlabsAndStairs;
+import net.frostbyte.slabsandstairs.block.ModBlocks;
 import net.minecraft.block.CraftingTableBlock;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
@@ -12,5 +13,5 @@ import net.minecraft.util.Identifier;
 public class ModItemGroup {
 
     public static final ItemGroup SLABSANDSTAIRS = FabricItemGroupBuilder.build(
-            new Identifier(SlabsAndStairs.MOD_ID, "slabsandstairs"), () -> new ItemStack(Items.CRAFTING_TABLE));
+            new Identifier(SlabsAndStairs.MOD_ID, "slabsandstairs"), () -> new ItemStack(ModBlocks.LIGHT_BLUE_CONCRETE_STAIRS));
 }
