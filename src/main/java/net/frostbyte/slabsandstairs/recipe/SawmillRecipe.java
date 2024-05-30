2e5a96886404aa23c834d0e6514c0899c9a63af9
FrostbyteGames <109047591+FrostbyteGames1@users.noreply.github.com>
2024-05-30 14:09:26 -0400

    Fixed "unknown recipe category"

--- src/main/java/net/frostbyte/slabsandstairs/recipe/SawmillRecipe.java
+++ src/main/java/net/frostbyte/slabsandstairs/recipe/SawmillRecipe.java
@@ -3,10 +3,12 @@ package net.frostbyte.slabsandstairs.recipe;
 import net.frostbyte.slabsandstairs.block.ModBlocks;
 import net.minecraft.inventory.Inventory;
 import net.minecraft.item.ItemStack;
-import net.minecraft.recipe.*;
+import net.minecraft.recipe.CuttingRecipe;
+import net.minecraft.recipe.Ingredient;
 import net.minecraft.world.World;
 
-import static net.frostbyte.slabsandstairs.recipe.ModRecipes.*;
+import static net.frostbyte.slabsandstairs.recipe.ModRecipes.SAWMILL_RECIPE;
+import static net.frostbyte.slabsandstairs.recipe.ModRecipes.SAWMILL_SERIALIZER;
 
 public class SawmillRecipe extends CuttingRecipe {
     public SawmillRecipe(String group, Ingredient ingredient, ItemStack result) {
@@ -19,5 +21,10 @@ public class SawmillRecipe extends CuttingRecipe {
 
     public ItemStack createIcon() {
         return new ItemStack(ModBlocks.SAWMILL);
+    }
+
+    @Override
+    public boolean isIgnoredInRecipeBook() {
+        return true;
     }
 }
